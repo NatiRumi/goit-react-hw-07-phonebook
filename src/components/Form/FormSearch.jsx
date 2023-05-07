9be34fe9fbf977320ed6ee6f7f7c464188df2e5c
NatiRumi <NatiRumiantseva@gmail.com>
2023-05-07 20:21:19 +0300

    change

--- src/components/Form/FormSearch.jsx
+++ src/components/Form/FormSearch.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Form.module.css';
 
-class FormSearch extends Component {
-  inputChange = e => {
-    e.preventDefault();
-    this.props.onChange(e.currentTarget.value);
-  };
+// class FormSearch extends Component {
+//   inputChange = e => {
+//     e.preventDefault();
+//     this.props.onChange(e.currentTarget.value);
+//   };
 
-  render() {
-    return (
-      <label className={css.formSearch}>
-        Find contacts by name
-        <input type="text" name="filter" onChange={this.inputChange} className={css.inputFormSearch}/>
-      </label>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <label className={css.formSearch}>
+//         Find contacts by name
+//         <input type="text" name="filter" onChange={this.inputChange} className={css.inputFormSearch}/>
+//       </label>
+//     );
+//   }
+// }
+
+const FormSearch = ({ onChange }) => {
+  return (
+    <label className={css.formSearch}>
+      Find contacts by name
+      <input
+        type="text"
+        name="filter"
+        onChange={e => onChange(e.currentTarget.value)}
+        className={css.inputFormSearch}
+      />
+    </label>
+  );
+};
 
 export default FormSearch;
 
