817d965996a0b2c34d4944b4ace82444c40af943
NatiRumi <NatiRumiantseva@gmail.com>
2023-05-10 20:59:46 +0300

    localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,15 +9,28 @@ import css from './Form/Form.module.css';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    // names: '',
+    // names: '',s
     // number: ''
   };
+
+  componentDidMount() {
+		const localData = localStorage.getItem('contacts')
+		if (localData) {
+			this.setState({ contacts: JSON.parse(localData) })
+		}
+	}
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+			localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+		}
+  }
 
   addItem = data => {
     let contact = {
