3dded1d6da7b735279ca2a589754d20e089ea0b1
NatiRumi <NatiRumiantseva@gmail.com>
2023-05-17 22:09:12 +0300

    local storage

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,69 +1,69 @@
 import React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './Form.module.css';
 
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+function Form({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleInputName = e => {
+    const { value } = e.currentTarget;
+    setName(value);
   };
 
-  handleInputChange = e => {
-    // console.log(e.currentTarget.value)
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-    // this.setState({ name: e.currentTarget.value });
+  const handleInputNumber = e => {
+    const { value } = e.currentTarget;
+    setNumber(value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    // console.log(this.state);
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={css.form}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleInputChange}
-            className={css.formInput}
-          />
-        </label>
-        <label>
-          Telephone
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handleInputChange}
-            className={css.formInput}
-          />
-        </label>
-        <button className={css.formBtn} type="submit">Add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} className={css.form}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleInputName}
+          className={css.formInput}
+        />
+      </label>
+      <label>
+        Telephone
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleInputNumber}
+          className={css.formInput}
+        />
+      </label>
+      <button className={css.formBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 export default Form;
-
 Form.propTypes = {
   onSubmit: PropTypes.func,
 };
--- src/components/Form/FormSearch.jsx
+++ src/components/Form/FormSearch.jsx
@@ -1,23 +1,6 @@
 import React from 'react';
-// import { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Form.module.css';
-
-// class FormSearch extends Component {
-//   inputChange = e => {
-//     e.preventDefault();
-//     this.props.onChange(e.currentTarget.value);
-//   };
-
-//   render() {
-//     return (
-//       <label className={css.formSearch}>
-//         Find contacts by name
-//         <input type="text" name="filter" onChange={this.inputChange} className={css.inputFormSearch}/>
-//       </label>
-//     );
-//   }
-// }
 
 const FormSearch = ({ onChange }) => {
   return (
